4fad7ea02d164fbd8090b58645b4e775b2f5e2a7
kaussy6902 <mu.kaustav@gmail.com>
2020-10-22 16:46:09 +0530

    upload autoexec

---
+++ Autoexec/Modules.bat
@@ -0,0 +1,7 @@
+pip install --upgrade scipy
+pip install --upgrade matplotlib
+pip install --upgrade seaborn
+pip install --upgrade sklearn
+pip install --upgrade statsmodels
+pip install --upgrade pandas
+pip install --upgrade numpy
---
+++ Autoexec/OPEN JUPYTER.bat
@@ -0,0 +1,3 @@
+D:
+cd D:\MATERIAL\ML\DATA SCIENCE
+jupyter notebook
