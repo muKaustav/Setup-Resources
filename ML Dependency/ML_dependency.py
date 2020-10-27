7a49296426250601ceff981b4fbd2e852bcc9b27
kaussy6902 <mu.kaustav@gmail.com>
2020-10-27 09:12:18 +0530

    ml lib update #1

--- ML Dependency/ML_dependency.py
+++ ML Dependency/ML_dependency.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -7,9 +8,12 @@ sns.set()
 
 from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import f_regression as freg
+from sklearn.preprocessing import StandardScaler as stdsclr
+
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 
-def adj_r2(x, y):
+def adj_r2(x,y):
     r2 = reg.score(x, y)
     n = x.shape[0]
     p = x.shape[1]
@@ -18,23 +22,23 @@ def adj_r2(x, y):
     return result
 
 
-def data_summary(x, y):
+def data_summary(x,y):
     r2_score = reg.score(x, y).round(3)
     adj_r2_score = adj_r2(x, y).round(3)
-    intercept = reg.intercept_.round(5)
+    bias = reg.intercept_.round(5)
     print("R2 value is", end=" ")
     print(r2_score, end="\n")
     print("Adjusted R2 value is", end=" ")
     print(adj_r2_score, end="\n")
-    print("Intercept value is", end=" ")
-    print(intercept, end="\n")
+    print("Bias value is", end=" ")
+    print(bias, end="\n")
 
 
-def feature_summary(x, y):
-    feature = pd.DataFrame(data=x.columns.values, columns=['Features'])
-    feature['Coefficients'] = reg.coef_.round(3)
+def feature_summary(x,y):
+    feature = pd.DataFrame([['SAT'],['Rand 1,2,3']], columns=['Features'])
+    feature['Weights'] = reg.coef_.round(3)
     feature['P_values'] = freg(x, y)[1].round(3)
-    print(feature)
+    return feature
 
 # def values(x,y):
 
